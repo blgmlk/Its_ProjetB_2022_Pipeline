796f5990143f3b696a590f1772dc8ce07c214bf7
DevJavu_tour <devJavu@mail.com>
2022-12-09 14:20:09 +0100

    page index avec récupération de variable venant du code et gestion des erreurs de status

--- src/main/java/com/compagny/myapp/MyErrorController.java
+++ src/main/java/com/compagny/myapp/MyErrorController.java
@@ -1,20 +1,42 @@
 package com.compagny.myapp;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
-public class MyErrorController implements ErrorController{
+public class MyErrorController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
-    public String error() {
-        return "Error handling";
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        // TODO: 2021-09-16 Ajouter une page d'erreur personalisé avec renvoie sur la page d'accueil et suppréssion de la session de la session en cours
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error-400";
+            } else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+                return "error-401";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error-403";
+            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+
+        return "error contatez moi ou openai ;p";
     }
 
     // @Override
     // public String getErrorPath() {
-    //     return PATH;
+    // return PATH;
     // }
 }
